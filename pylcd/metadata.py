4f41a0a3a6e9ac63087953e34eef6ba36ef20a35
Julian Metzler <git@mezgr.de>
2015-08-08 13:03:34 +0200

    Fixed PIL dependency

--- pylcd/metadata.py
+++ pylcd/metadata.py
@@ -2,11 +2,11 @@
 # See the LICENSE file for the full license.
 
 name = "pyLCD"
-version = "1.1.2"
+version = "1.1.3"
 description = "A library for controlling LCDs on various hardware backends"
 license = "AGPLv3"
 author = "Julian Metzler"
 author_email = "pylcd@mezgr.de"
-dependencies = []
+requires = ['PIL']
 url = "https://github.com/Mezgrman/pyLCD"
 keywords = "library lcd display hd44780 ks0108 interface"
